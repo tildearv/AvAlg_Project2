c0443424258d5fd59f0309f4c10b602a05436cac
tarvedson <tilde.arvedson@gmail.com>
2016-11-17 11:33:48 +0100

    pollard, some probs, added in readme

--- main.cpp
+++ main.cpp
@@ -35,39 +35,39 @@ int main(int argc, char* argv[]) {
         //gmp_printf("%s is an mpz %Zd\n", "here",number);
         int prob = is_prime(number, 50);
 
-        if(prob > 0){
+        if(prob == 2){
             gmp_printf("%Zd \n", number);
         }else{
 
-        //solve_pollard(number, 2, 2, 1); 
-        vector<int> factors = floyd(number, 2, 2); 
+            //solve_pollard(number, 2, 2, 1); 
+            //vector<int> factors = floyd(number, 2, 2); 
 
-        //vector<int> factors = naive_factoring(number);
+            vector<int> factors = naive_factoring(number);
 
-        //gmp_printf("%d \n", prob);
+            //gmp_printf("%d \n", prob);
 
-        //vector<int> factors = divide_by_first_primes(number);
-        unsigned long int prod = 1;
-        mpz_t rest;
-        mpz_init_set(rest, number);
-        for(int v = 0; v < factors.size(); ++v){
-            prod *= factors[v];
-            mpz_divexact_ui (rest, rest, factors[v]);
-        }
-        int prob_rest = is_prime(rest, 50);
-        if(mpz_cmp_d(number, prod) <= 0){ //if the product of the factors isn't equal to the number
+            //vector<int> factors = divide_by_first_primes(number);
+            unsigned long int prod = 1;
+            mpz_t rest;
+            mpz_init_set(rest, number);
             for(int v = 0; v < factors.size(); ++v){
-                cout<<factors[v]<<endl;
+                prod *= factors[v];
+                mpz_divexact_ui (rest, rest, factors[v]);
             }
-        }
-        /*else if(prob_rest > 0){
-            for(int v = 0; v < factors.size(); ++v){
-                cout<<factors[v]<<endl;
+            int prob_rest = is_prime(rest, 50);
+            if(mpz_cmp_d(number, prod) <= 0){ //if the product of the factors isn't equal to the number
+                for(int v = 0; v < factors.size(); ++v){
+                    cout<<factors[v]<<endl;
+                }
             }
-            gmp_printf("%Zd \n", rest);
-        }*/
-        else{cout<<"fail"<<endl;}
-        cout<<endl;
+            /*else if(prob_rest > 0){
+                for(int v = 0; v < factors.size(); ++v){
+                    cout<<factors[v]<<endl;
+                }
+                gmp_printf("%Zd \n", rest);
+            }*/
+            else{cout<<"fail"<<endl;}
+            cout<<endl;
         }
     }
 
